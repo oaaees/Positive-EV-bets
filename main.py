6c6d87745b18f43882bf9b439ece0d953ce38828
Johan Paredes <jmpareds@gmail.com>
2023-11-13 21:27:19 -0400

    exports as csv

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-urls = ['https://www.pinnacle.com/es/soccer/england-premier-league/matchups/']
-driver = webdriver.Chrome()
-driver.implicitly_wait(10)
-
-pages = []
-
-for url in urls:
-    driver.get(url)
-    main = driver.find_element(By.TAG_NAME, "main")
-    pages.append(main.find_element(By.CSS_SELECTOR,".contentBlock.square"))
-
 class Match:
     def __init__(self, team1, team2, date, time, p1, ptie, p2):
         self.team1 =  team1
@@ -22,30 +11,70 @@ class Match:
         self.ptie = ptie
         self.p2 = p2
     def print(self) :
-        print(self.team1 + " vs " + self.team2 + " " + self.p1 + " - " + self.ptie + " - " + self.p2)
+        print(f'{self.team1} vs {self.team2} with prob {self.p1:.5f} - {self.ptie:.5f} - {self.p2:.5f}')
+    def printCSV(self) :
+        return f'{self.team1},{self.team2},{self.p1:.5f},{self.ptie:.5f},{self.p2:.5f},,,,{self.date}\n'
 
-Matches = []
 
-for page in pages:
-    divs = page.find_elements(By.TAG_NAME, "div")
-    date = ""
+#pinnacle_urls = ['https://www.pinnacle.com/es/soccer/spain-la-liga/matchups/',
+#                 'https://www.pinnacle.com/es/soccer/england-premier-league/matchups/',
+#                 'https://www.pinnacle.com/es/soccer/italy-serie-a/matchups/',
+#                 'https://www.pinnacle.com/es/soccer/germany-bundesliga/matchups/',
+#                 'https://www.pinnacle.com/es/soccer/fifa-world-cup-qualifiers-south-america/matchups/',
+#                 'https://www.pinnacle.com/es/soccer/uefa-euro-qualifiers/matchups/',
+#                 'https://www.pinnacle.com/es/soccer/uefa-champions-league/matchups/',
+#                 ]
 
-    for div in divs:
-        is_date = "style_dateBar__1adEH" in div.get_attribute("class")
-        is_spacer = "style_row__3l5MS" in div.get_attribute("class")
-        is_match = "style_row__12oAB" in div.get_attribute("class")
+pinnacle_urls = ['https://www.pinnacle.com/es/soccer/fifa-world-cup-u17/matchups']
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
 
-        if is_date: date = div.text
-        if is_spacer: continue
-        if is_match:
-            teams = div.find_elements(By.CLASS_NAME, "style_gameInfoLabel__2m_fI")
-            time = div.find_element(By.CLASS_NAME, "style_matchupDate__UG-mT")
-            p = div.find_elements(By.CLASS_NAME, "style_button-wrapper__2u2GV")
+def get_matches(pinnacle_urls) :
+    Matches = []
 
-            Matches.append(Match(teams[0].text.replace('(Partido)', ''), teams[1].text.replace('(Partido)', ''), date, time.text, p[0].text, p[1].text, p[2].text))
+    for url in pinnacle_urls:
+        driver.get(url)
+        main = driver.find_element(By.TAG_NAME, "main")
+        page = main.find_element(By.CSS_SELECTOR,".contentBlock.square")
 
-for m in Matches:
-    m.print()
+        divs = page.find_elements(By.TAG_NAME, "div")
+        date = ""
+
+        for div in divs:
+            is_date = "style_dateBar__1adEH" in div.get_attribute("class")
+            is_spacer = "style_row__3l5MS" in div.get_attribute("class")
+            is_match = "style_row__12oAB" in div.get_attribute("class")
+
+            if is_date: date = div.text
+            if is_spacer: continue
+            if is_match:
+                teams = div.find_elements(By.CLASS_NAME, "style_gameInfoLabel__2m_fI")
+                time = div.find_element(By.CLASS_NAME, "style_matchupDate__UG-mT")
+                p = div.find_elements(By.CLASS_NAME, "style_button-wrapper__2u2GV")
+
+                sum = 0
+                for i in range(len(p)) : 
+                    if p[i].text == '' : 
+                        p[i] = 0
+                        continue
+                    p[i] = float(p[i].text)
+                    p[i] = (1 / p[i]) * 100 
+                    sum += p[i]
+                for i in range(len(p)) :
+                    if sum == 0 : break
+                    p[i] /= sum
+
+                Matches.append(Match(teams[0].text.replace(' (Partido)', ''), teams[1].text.replace(' (Partido)', ''), date.replace(',',''), time.text, p[0], p[1], p[2]))
+    return Matches
+
+Matches = get_matches(pinnacle_urls)
+
+f = open("results.csv", "w")
+
+for match in Matches :
+    f.write(match.printCSV())
+
+f.close()
 
 
 
